6e4d9d637b0453052ac08bac906430e0f1208d5f
petrakiio <eup0022@gmail.com>
2026-02-21 18:57:26 -0500

    Adicionando meu codigo feito,com base no video

--- Estudos/esp32.py
+++ Estudos/esp32.py
@@ -4,3 +4,16 @@
 #link curso:https://youtu.be/P6qUlAc8KaU?si=CHYF1T8uZCPa0Kuf
 #tutorial da extensão:https://youtu.be/35a4z4X2XZI?si=ZU_PzXMTmhnDFB18
 
+#Codigo do video
+from machine import Pin #Importando os pinos
+from time import sleep #usado pro delay
+
+ledRed = Pin(4, Pin.OUT) # Definindo o pino 4 como saida
+
+while True:
+  ledRed.value(1) #Liga o led: 1 > true
+  sleep(2) # Espera 1 segundo
+  ledRed.value(0) # Desliga o led: 0 > false
+  sleep(2)
+
+#Meu codigo
