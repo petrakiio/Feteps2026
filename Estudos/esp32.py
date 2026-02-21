320a383b769011a449ff37500ca3451e3d244c64
petrakiio <eup0022@gmail.com>
2026-02-21 18:27:26 -0500

    Configurando ambiente de programação

--- Estudos/esp32.py
+++ Estudos/esp32.py
@@ -1,3 +1,6 @@
 #Comencando estudo no ESP32
 #Site usado:https://wokwi.com/?utm_source=chatgpt.com
 #EXtensão:https://docs.wokwi.com/vscode/getting-started
+#link curso:https://youtu.be/P6qUlAc8KaU?si=CHYF1T8uZCPa0Kuf
+#tutorial da extensão:https://youtu.be/35a4z4X2XZI?si=ZU_PzXMTmhnDFB18
+
