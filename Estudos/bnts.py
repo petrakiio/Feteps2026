221475d6977ea8afa2e929a16adb7cf05ac37ae4
petrakiio <eup0022@gmail.com>
2026-02-21 21:05:24 -0500

    Codigo funcional

--- Estudos/bnts.py
+++ Estudos/bnts.py
@@ -1,12 +1,26 @@
-from machine import Pin
+from machine import Pin,I2C #importando a tela
+import ssd1306 #biblioteca da tela oled
+
+#Variaveis da tela 
+i2c = I2C(0, scl=Pin(22), sda=Pin(21)) #Cria barramento
+oled = ssd1306.SSD1306_I2C(128, 64, i2c) #Cria obj(objeto) da tela
 
 #Controlando buttons
 btn = Pin(19,Pin.IN,Pin.PULL_UP)
 ledRed = Pin(16,Pin.OUT)
 
-#Em desenvolvimento
-while True:
+def escreverText():
+    oled.fill(0)
+    oled.text('Voce clicou!',0,0)
+    oled.show()
+
+def click():
     if btn.value() == 0:
+        escreverText()
         ledRed.value(1)
     else:
         ledRed.value(0)
+
+while True:
+    click()
+   
