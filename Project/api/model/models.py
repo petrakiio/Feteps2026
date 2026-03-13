c60504f4a9bd451b430fe3d222089f0dda592183
petrakiio <eup0022@gmail.com>
2026-03-12 19:27:41 -0500

    Bug de path resolvido

--- Project/api/model/models.py
+++ Project/api/model/models.py
@@ -1,3 +1,6 @@
-from login.user import User
+from model.login.user import User
+from model.login.doctor import Doctor
+from model.login.instituicao import Instituicao
+from model.login.cuidador import Cuidador
 
-__all__ = ["User"]
+__all__ = ["User", "Doctor", "Instituicao", "Cuidador"]
